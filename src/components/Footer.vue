dc64b080fbf12c6429d593b01bb1ab4e1d9104ca
WalterFrongia <walter.frongia@libero.it>
2021-10-08 15:27:49 +0200

    Implementata sezione informazioni in footer component con aggiunta di logo, testo, title e img social

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,36 @@
 <template>
     <div id="footer-section">
         <div class="container">
+
             <div class="row">
                 <!-- INFORMAZIONI AVADA HEALT FOOTER -->
                 <div class="col-4">
-
+                    <div class="row">
+                        <div class="col-7 pb-3">
+                            <img src="../assets/images/medical_logo_1x_dark.png" alt="">
+                        </div>
+                    
+                        <div class="col-12">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                audantium, totam rem aperiam, eaque ipsa quae ab illo inventore vritatis et quasi
+                                architecto beatae
+                            </p>
+                        </div>
+                        <div class="col-12 pt-2">
+                            <div class="my-line"></div>
+                        </div>
+                        <div class="col-12">
+                            <h5>LEARN MORE ABOUT US</h5>
+                        </div>
+                        <div clas="row">
+                            <div class="col-5 d-flex pt-3">
+                                <img src="../assets/images/logo-facebook.png" alt="">
+                                <img src="../assets/images/logo-twitter.png" alt="">
+                                <img src="../assets/images/instagram.jpg" alt="">
+                                <img src="../assets/images/logo-youtube.png" alt="">
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- /INFORMAZIONI AVADA HEALT FOOTER -->
                 
@@ -15,11 +41,12 @@
                 <!-- / CONTACT INFORMATION FOOTER -->
 
                 <!-- MEDICAL DEPARTMENTS FOOTER -->
-                <div clas="col-4">
+                <div class="col-4">
 
                 </div>
                 <!-- /MEDICAL DEPARTMENTS FOOTER -->
             </div>
+
         </div>
     </div>
 </template>
@@ -35,6 +62,24 @@ export default {
 @import "../style/variables.scss";
 
 #footer-section{
+    padding-top:50px;
+
     height: 500px;
+
+    p{
+        line-height: 35px;
+    }
+
+    img{
+        width:100%;
+
+        border-radius: 10px;
+        margin:0 5px;
+    }
+}
+
+.my-line{
+    border:1px solid #bcbcbd;
+    border-radius: 10px;
 }
 </style>
