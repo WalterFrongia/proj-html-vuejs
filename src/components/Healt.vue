45c7f6743d00d2a7be1323dd54c8bf493d6ec5f2
WalterFrongia <walter.frongia@libero.it>
2021-10-08 09:52:17 +0200

    Implementazione layout icona titolo e descrizione in Healt component

--- src/components/Healt.vue
+++ src/components/Healt.vue
@@ -1,5 +1,29 @@
 <template>
     <div id="healt-section">
+        <!-- SEZIONE ICONA TITOLO E DESCRIZIONE HEALT SECTION -->
+        <div class="row pt-5">
+            <div class="col-12 text-center">
+                <img src="../assets/images/icon-6.png" alt="">
+            </div>
+        </div>
+        <div class="row pt-3">
+            <div class="col-12 text-center">
+                <h1>OUR HEALT SERVICES</h1>
+            </div>
+        <div class="row d-flex justify-content-center pt-3">
+            <div class="col-3 my-line"></div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-8 pt-3 text-center">
+                <p>Sed ut perspiaciatis omnis iste natus error sit voluptatem accusantium
+                    doloremque laudatium, totam rem apreriam, aeque ipsa quae ab illo
+                    inventore veritatis et quasi architetto beatae
+                </p>
+            </div>
+        </div>
+        <!-- / SEZIONE ICONA TITOLO E DESCRIZIONE HEALT SECTION -->
+    </div>
+            
     </div>
 </template>
 
@@ -13,6 +37,16 @@ export default {
 @import "../style/variables.scss";
 
 #healt-section{
+    margin:auto;
+    width:$primaryWidth;
     height: 500px;
+    p{
+        line-height: 35px;
+    }
+}
+
+.my-line{
+    border:1px solid #bcbcbd;
+    border-radius: 10px;
 }
 </style>
