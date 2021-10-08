99a49ea370b1ae69b6f6accdc8e31cfd28cf05a9
WalterFrongia <walter.frongia@libero.it>
2021-10-08 10:30:03 +0200

    Implementato bg image in Facilities component

--- src/components/Facilities.vue
+++ src/components/Facilities.vue
@@ -1,5 +1,9 @@
 <template>
-    <div id="my-facilities-section"></div>
+    <div id="my-wrapping-facilities">
+        <div id="my-facilities-section">
+
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,4 +15,15 @@ export default {
 <style lang="scss" scoped>
 @import "../style/variables.scss";
 
+#my-wrapping-facilities{
+    background-image: url(../assets/images/paralax-bg-tour-facilities.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    
+    padding-top: 50px;
+}
+#my-facilities-section{
+    height: 500px;
+}
 </style>
