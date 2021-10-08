adb265a64f0e89bdde6cdd331e73181e035065de
WalterFrongia <walter.frongia@libero.it>
2021-10-08 12:38:45 +0200

    Implementato titolo con barra e descrizione in Appointment component

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -1,7 +1,22 @@
 <template>
     <div id="my-wrapping-appointment-section">
         <div id="my-appointment-section">
-
+            <div class="row d-flex justify-content-center">
+                <div class="col-6">
+                    <h1>Make An Appointment</h1>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center pt-3 pb-4">
+                <div class="col-3 my-line"></div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-7 text-center">
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore vritatis et quasi
+                        architecto beatae
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -22,6 +37,16 @@ export default {
     background-position: center;
 }
 #my-appointment-section{
+    margin:auto;
+    width:$primaryWidth;
+
     height: 500px;
+    color:white;
+    padding-top:100px;
+}
+
+.my-line{
+    border:1px solid #bcbcbd;
+    border-radius: 10px;
 }
 </style>
